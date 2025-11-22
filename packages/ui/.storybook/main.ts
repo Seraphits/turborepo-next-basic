2bd483a8a6c52589638598d147dbf5623ab38698
Seraphits <97304529+Seraphits@users.noreply.github.com>
2025-11-21 20:05:19 -0500

    Storybook

---
+++ packages/ui/.storybook/main.ts
@@ -0,0 +1,28 @@
+import type { StorybookConfig } from '@storybook/react-webpack5';
+
+import { dirname } from "path"
+
+import { fileURLToPath } from "url"
+
+/**
+* This function is used to resolve the absolute path of a package.
+* It is needed in projects that use Yarn PnP or are set up within a monorepo.
+*/
+function getAbsolutePath(value: string): any {
+  return dirname(fileURLToPath(import.meta.resolve(`${value}/package.json`)))
+}
+const config: StorybookConfig = {
+  "stories": [
+    "../src/**/*.mdx",
+    "../src/**/*.stories.@(js|jsx|mjs|ts|tsx)"
+  ],
+  "addons": [
+    getAbsolutePath('@storybook/addon-webpack5-compiler-swc'),
+    getAbsolutePath('@storybook/addon-docs')
+  ],
+  "framework": {
+    "name": getAbsolutePath('@storybook/react-webpack5'),
+    "options": {}
+  }
+};
+export default config;
