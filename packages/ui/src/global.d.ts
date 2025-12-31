17e804d462374429040196dda9108447b05b2646
Seraphits <97304529+Seraphits@users.noreply.github.com>
2025-12-31 13:15:18 -0500

    fix(config): switch ui package to bundler resolution to resolve TS2835

---
+++ packages/ui/src/global.d.ts
@@ -0,0 +1,8 @@
+declare module '*.scss' {
+  const content: Record<string, string>;
+  export default content;
+}
+declare module '*.module.scss' {
+  const classes: { readonly [key: string]: string };
+  export default classes;
+}
