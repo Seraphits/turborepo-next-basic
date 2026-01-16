737033a7ba13a0e4f8c1f08af584fadd3e28bb2b
Seraphits <97304529+Seraphits@users.noreply.github.com>
2026-01-15 21:59:55 -0500

    Fix Backgrroud to light

--- apps/scrambled-sounds/app/page.tsx
+++ apps/scrambled-sounds/app/page.tsx
@@ -1,50 +1,10 @@
-'use client';
+"use client";
 
-import styled from 'styled-components';
 
-// 1. "Restorative Green" Background (Dorsal)
-const Container = styled.main`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  justify-content: center;
-  min-height: 100vh;
-  padding: 4rem;
-  background-color: ${props => props.theme.colors.background};
-  color: ${props => props.theme.colors.surface};
-`;
-
-// 2. "Henny Penny" Font (Playful Header)
-const Title = styled.h1`
-  font-family: ${props => props.theme.typography.displayFont};
-  font-size: 3.5rem;
-  color: ${props => props.theme.colors.surface};
-  margin-bottom: 2rem;
-  text-shadow: 2px 2px 0px ${props => props.theme.colors.primary};
-`;
-
-// 3. "Stimulus Orange" Button (Ventral)
-const CalmButton = styled.button`
-  padding: 1.2rem 3rem;
-  font-size: 1.5rem;
-  font-family: ${props => props.theme.typography.bodyFont}; // Lexend for readability
-  background-color: ${props => props.theme.colors.primary};
-  color: ${props => props.theme.colors.text};
-  border: none;
-  border-radius: ${props => props.theme.utils.borderRadius};
-  cursor: pointer;
-  transition: transform 0.2s ease-out;
-
-  &:hover {
-    transform: scale(1.05);
-  }
-`;
-
-export default function Page() {
+export default function Home() {
   return (
-    <Container>
-      <Title>Scrambled Sounds</Title>
-      <CalmButton>Start Listening</CalmButton>
-    </Container>
+    <div >
+      <h1> Scrambled Sounds Home Page</h1>
+    </div>
   );
 }
